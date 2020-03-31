eb5dc37a57b7ffd6d551c7f26632aac9c19e616d
Vanya <vanyavasylyshyn@gmail.com>
2020-03-31 15:39:05 +0300

    Make a basic User model (including secure passwords)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   include ApplicationHelper
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: 1)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
