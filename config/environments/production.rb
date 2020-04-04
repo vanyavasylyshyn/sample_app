aeb50a5537d82789e79bb166cf94a5ef181a3a52
Vanya <vanyavasylyshyn@gmail.com>
2020-04-04 20:55:41 +0300

    Configure producuction eviroment for Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '.herokuapp.com'
+  host = 'gentle-sands-10492.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer ::Base.smtp_settings = {
        :address        => 'smtp.sendgrid.net',
