5effa34ddeb96e92616820796c00eddc02d57539
Vanya <vanyavasylyshyn@gmail.com>
2020-04-05 13:01:59 +0300

    Done 50% of microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -10,6 +11,7 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
 
   # Returns the hash digest of the given string.
   def User.digest(string)
